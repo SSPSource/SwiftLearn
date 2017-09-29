ed76e4ba43e634b7d0272f224c5e1881a09dae66
Sunsp <diudiu2015@users.noreply.github.com>
2017-09-29 16:30:42 +0800

    Initial Commit

---
+++ SspSwift/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SspSwift
+//
+//  Created by admin on 2017/9/29.
+//  Copyright © 2017年 ssp. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SspSwiftTests/SspSwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  SspSwiftTests.swift
+//  SspSwiftTests
+//
+//  Created by admin on 2017/9/29.
+//  Copyright © 2017年 ssp. All rights reserved.
+//
+
+import XCTest
+@testable import SspSwift
+
+class SspSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
